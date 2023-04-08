d0565d2f500b0104148cf3959b658d74a2a7bd3c
Phillip Dickey <58786787+phillipjad@users.noreply.github.com>
2023-04-08 18:26:04 -0400

    Add files via upload
    Adds servo functionality to switch Humidifier on.  May transition to digital activation through relay-arduino functionality.

--- humidifier.ino
+++ humidifier.ino
@@ -1,45 +1,48 @@
 //Includes for default arduino libraries to interface
 //with digital humidity & temperature sensor (DHT11)
+// and SG90 servo
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
+#include <Servo.h>
 
 //defined pins for sensors
-#define HUM_VCC 7 //pin for Humidity sensor voltage
-#define HUM_DATA 3 //pin for Humidity sensor data 
-#define CHIP_SELECT 10 //pin for Thermistor chip select
-#define MISO 11 //pin that receives signals from thermistors
-#define THERMO_VCC 12 //pin that powers thermistors
-#define THERMO_SCK 13 //pin that syncs data exchange
+#define SERVO_DATA 8    //pin for Servo data
+#define HUM_VCC 7       //pin for Humidity sensor voltage
+#define HUM_DATA 3      //pin for Humidity sensor data
+#define CHIP_SELECT 10  //pin for Thermistor chip select
+#define MISO 11         //pin that receives signals from thermistors
+#define THERMO_VCC 12   //pin that powers thermistors
+#define THERMO_SCK 13   //pin that syncs data exchange
 
+Servo servo;
 
 //One-time initialization code before main looop drives device
 void setup() {
   //Humidity Sensor pin setup
-  pinMode(HUM_VCC, OUTPUT); //sets humidifiers voltage pin to output
-  digitalWrite(HUM_VCC, HIGH); //passes voltage through pin
-  //Thermistor pin setup 
-  pinMode(CHIP_SELECT, OUTPUT); //sets chip select pin to output
-  digitalWrite(CHIP_SELECT, HIGH); //passes voltage through pin
-  pinMode(MISO, INPUT); //sets MISO pin to input
-  pinMode(THERMO_VCC, OUTPUT); //sets humidifiers voltage pin to output
-  digitalWrite(THERMO_VCC, HIGH); //passes voltage through pin
-  pinMode(THERMO_SCK, OUTPUT); //sets humidifiers voltage pin to output
-  Serial.begin(9600); //Opens 9600 baud for output
+  pinMode(HUM_VCC, OUTPUT);     //sets humidifiers voltage pin to output
+  digitalWrite(HUM_VCC, HIGH);  //passes voltage through pin
+  //Thermistor pin setup
+  pinMode(CHIP_SELECT, OUTPUT);     //sets chip select pin to output
+  digitalWrite(CHIP_SELECT, HIGH);  //passes voltage through pin
+  pinMode(MISO, INPUT);             //sets MISO pin to input
+  pinMode(THERMO_VCC, OUTPUT);      //sets humidifiers voltage pin to output
+  digitalWrite(THERMO_VCC, HIGH);   //passes voltage through pin
+  pinMode(THERMO_SCK, OUTPUT);      //sets humidifiers voltage pin to output
+  Serial.begin(9600);               //Opens 9600 baud for output
 }
 
 //helper function to read signal from thermistor
-uint8_t readSig(){
+uint8_t readSig() {
   uint8_t temp = 0;
-  for (int i = 7; i >= 0; i--){
-    digitalWrite(THERMO_SCK, HIGH); //passes voltage through pin
-    if (digitalRead(MISO)){
+  for (int i = 7; i >= 0; i--) {
+    digitalWrite(THERMO_SCK, HIGH);  //passes voltage through pin
+    if (digitalRead(MISO)) {
       //if thermistor is attempting to pass signal it is
       //read into temp at proper bit
       temp |= (1 << i);
     }
-    digitalWrite(THERMO_SCK, LOW); //stops voltage through pin
+    digitalWrite(THERMO_SCK, LOW);  //stops voltage through pin
   }
   return temp;
 }
@@ -53,6 +56,15 @@ void loop() {
   //Initializes DHT11 sensor for usage
   DHT dht(HUM_DATA, DHT11);
   dht.begin();
+  servo.attach(SERVO_DATA);
+  for (int angle = 180; angle > 0; angle--) {
+    servo.write(angle);
+    delay(15);
+  }
+  for (int angle = 0; angle < 180; angle++) {
+    servo.write(angle);
+    delay(15);
+  }
   //prepares to read data from thermistor
   digitalWrite(CHIP_SELECT, LOW);
   delay(10);
@@ -67,7 +79,7 @@ void loop() {
   //and applies resolution
   temp = (signal >> 3) * 0.25;
   Serial.println("tempC: " + String(temp));
-  temp = temp * 9.0/5.0 + 32;
+  temp = temp * 9.0 / 5.0 + 32;
   Serial.println("tempF: " + String(temp));
   hum = dht.readHumidity();
   Serial.println("Humidity: " + String(hum) + "%");
